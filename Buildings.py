23db228b184245b25d8529bb8f4b534c77520e39
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-15 11:40:54 +0200

    Update Buildings.py

--- Buildings.py
+++ Buildings.py
@@ -1,43 +1,42 @@
 import Elevator
 
+
 class Building:
 
-    def __init__(self, minFloor, maxFloor) -> None:
-        self.minFloor = int(minFloor)
-        self.maxFloor = int(maxFloor)
+    def __init__(self, min_floor, max_floor) -> None:
+        self.min_floor = int(min_floor)
+        self.max_floor = int(max_floor)
         self.elevators = []
 
-    def getminFloor(self):
-        return self.minFloor
+    # Getters & Setters
 
-    def setminFloor(self, minFloor):
-        self.minFloor = minFloor
+    def get_min_floor(self):
+        return self.min_floor
 
-    def getmaxFloor(self):
-        return self.maxFloor
+    def set_min_floor(self, min_floor):
+        self.min_floor = min_floor
 
-    def setmaxFloor(self, maxFloor):
-        self.maxFloor = maxFloor
+    def get_max_floor(self):
+        return self.max_floor
 
+    def set_max_floor(self, max_floor):
+        self.max_floor = max_floor
 
-    def getEl(self):
+    def get_elev(self):
         return self.elevators
 
-
-    def setEl(self, elev: dict):
+    def set_elev(self, elev: dict):
         self.elevators = elev
 
-    #add the elevators to the list num of elevators
-    def addEl(self, elev: Elevator):
+    def add_elev(self, elev: Elevator):
         self.elevators.append(elev)
 
-    def toString(self):
+    def to_string(self):
         str2 = ""
-
         for x in self.elevators:
             str2 += "{"
             str2 += "{},\n".format(x.toString())
             str2 += "}"
-        str = "minFloor = {}, maxFloor = {}, numElevators = {},\n elevators = \n{}".format(
-            self.minFloor, self.maxFloor, len(self.elevators), str2)
-        return str
+        string = "min_floor = {}, max_floor = {}, numElevators = {},\n elevators = \n{}".format(
+            self.min_floor, self.max_floor, len(self.elevators), str2)
+        return string
