1c279cb25fba2ff7f53845e838ed69790ab77cfe
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-11-16 18:16:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import json
 import csv
 import sys
-import os.path
 from Calls import Calls
 from Building import Building
 from Elevator import Elevator
-import numpy as np
 
 listCalls = []
 listElev = []
@@ -22,11 +20,11 @@ def find_optimal_elev(call: Calls):
     for elev in listElev:
         find_direction(elev, elev.get_curr_floor(), call.get_src())
         arrival_src_time = elev.calc_elevator(elev.get_curr_floor(), call.get_src())
+
         find_direction(elev, call.get_src(), call.get_dest())
         arrival_dest_time = elev.calc_elevator(call.get_src(), call.get_dest())
+
         total_time = arrival_src_time + arrival_dest_time
-        # print(total_time)
-        # print(min_time)
         if total_time < min_time:
             min_time = total_time
             close_elev = elev
@@ -51,17 +49,13 @@ def read_csv(file: str) -> list:
         csv_reader = csv.reader(csv_list)
         for row in csv_reader:
             c = Calls(row[1], row[2], row[3])
-            # print(c)
             list_csv.append(c)
-        # for val in listCalls:
-        #     print(val.toString())
     return list_csv
 
 
 # list back to file
 
 def write_csv(file: str, new_list: list) -> None:  # new_list = calls (speed,
-    # print(new_list)
     with open(file, 'w') as csv_file:
         for i in new_list:
             csv_file.write(i + '\n')
@@ -74,7 +68,6 @@ def read_json(file: str) -> Building:
         json_data = json.load(json_file)
         _minFloor = int(json_data['_minFloor'])
         _maxFloor = int(json_data['_maxFloor'])
-        check_length = _maxFloor - _minFloor + 1
         building = Building(_minFloor, _maxFloor)
         for elev in json_data['_elevators']:
             _id = elev["_id"]
@@ -86,7 +79,6 @@ def read_json(file: str) -> Building:
             _startTime = elev["_startTime"]
             _stopTime = elev["_stopTime"]
             elevator = Elevator(_id, _minFloor, _maxFloor, _speed, _closeTime, _openTime, _startTime, _stopTime)
-            # elevator = Elevator(**elev)
             building.add_elev(elevator)
         return building
 
@@ -108,29 +100,21 @@ def main():
     listElev = b.get_elev()
     expected_calls = []
     calls = read_csv(CALLS)
-    # print(os.getcwd())
     if len(listElev) == 0:
         print("There is no elevators in this building!")
     elif len(listElev) == 1:
         for call in calls:
-            # call.set_elev(listElev[0])
             elevator = calc(call)
             expected_calls.append(call_to_string(call, elevator.get_id()))
-        write_csv(OUTPUT, expected_calls)  # Calc_B.csv
+        write_csv(OUTPUT, expected_calls)
         return
 
     for call in calls:
-        # call.set_elev(calc(call))
-
         elevator = calc(call)
-        # print(elevator.get_id())
         expected_calls.append(call_to_string(call, elevator.get_id()))
         for i in listElev:
             if i.get_id() == elevator.get_id():
                 i.set_curr_floor(call.get_dest())
-
-    #     call[5] = calc(call).get_id()
-    #     print(call)
     write_csv(OUTPUT, expected_calls)
 
 
